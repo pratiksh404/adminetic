4fdcf93773ae71747d1ed2da6da17d1775f1d2b5
pratiksh404 <pratikdai404@gmail.com>
2022-03-18 14:02:24 +0545

    global wrapper class and id config setting

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -16,40 +16,42 @@
 </head>
 
 <body>
-    {{-- Loading Spinner --}}
-    @if (loader_enabled())
-    <div class="loader-wrapper">
-        <div class="loader-index"><span></span></div>
-        <svg>
-            <defs></defs>
-            <filter id="goo">
-                <fegaussianblur in="SourceGraphic" stddeviation="11" result="blur"></fegaussianblur>
-                <fecolormatrix in="blur" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo">
-                </fecolormatrix>
-            </filter>
-        </svg>
-    </div>
-    @endif
-    {{-- Loading Spinner End --}}
-    <!-- tap on top starts-->
-    <div class="tap-top"><i data-feather="chevrons-up"></i></div>
-    <!-- tap on tap ends-->
-    <!-- page-wrapper Start-->
-    <div class="page-wrapper compact-wrapper" id="pageWrapper">
-        <!-- Page Header Start-->
-        @includeFirst(['admin.layouts.components.header','adminetic::admin.layouts.components.header'],Adminetic::getHeaderData())
-        <!-- Page Header Ends                              -->
-        <!-- Page Body Start-->
-        <div class="page-body-wrapper">
-            <!-- Page Sidebar Start-->
-            @include('adminetic::admin.layouts.components.sidebar')
-            <!-- Page Sidebar Ends-->
-            <div class="page-body">
-                {{-- CONTENT --}}
-                @yield('content')
+    <div id="{{config('adminetic.wrapper_id','app')}}" class="{{config('adminetic.wrapper_class','app')}}">
+        {{-- Loading Spinner --}}
+        @if (loader_enabled())
+        <div class="loader-wrapper">
+            <div class="loader-index"><span></span></div>
+            <svg>
+                <defs></defs>
+                <filter id="goo">
+                    <fegaussianblur in="SourceGraphic" stddeviation="11" result="blur"></fegaussianblur>
+                    <fecolormatrix in="blur" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo">
+                    </fecolormatrix>
+                </filter>
+            </svg>
+        </div>
+        @endif
+        {{-- Loading Spinner End --}}
+        <!-- tap on top starts-->
+        <div class="tap-top"><i data-feather="chevrons-up"></i></div>
+        <!-- tap on tap ends-->
+        <!-- page-wrapper Start-->
+        <div class="page-wrapper compact-wrapper" id="pageWrapper">
+            <!-- Page Header Start-->
+            @includeFirst(['admin.layouts.components.header','adminetic::admin.layouts.components.header'],Adminetic::getHeaderData())
+            <!-- Page Header Ends                              -->
+            <!-- Page Body Start-->
+            <div class="page-body-wrapper">
+                <!-- Page Sidebar Start-->
+                @include('adminetic::admin.layouts.components.sidebar')
+                <!-- Page Sidebar Ends-->
+                <div class="page-body">
+                    {{-- CONTENT --}}
+                    @yield('content')
+                </div>
+                <!-- footer start-->
+                @includeFirst(['admin.layouts.components.footer','adminetic::admin.layouts.components.footer'],Adminetic::getFooterData())
             </div>
-            <!-- footer start-->
-            @includeFirst(['admin.layouts.components.footer','adminetic::admin.layouts.components.footer'],Adminetic::getFooterData())
         </div>
     </div>
     {{-- ASSET SCRIPTS --}}
