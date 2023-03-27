6259dda16836ece013aa5b8b74597332f1f475c6
DRH2SO4 <pratikdai404@gmail.com>
2023-03-27 14:28:14 +0545

    data table

---
+++ database/migrations/2021_05_24_000001_create_data_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+}
---
+++ src/Models/Admin/Data.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Pratiksh\Adminetic\Models\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Data extends Model
+{
+    use LogsActivity;
+
+    protected $guarded = [];
+
+    // Forget cache on updating or saving and deleting
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function () {
+            self::cacheKey();
+        });
+
+        static::deleting(function () {
+            self::cacheKey();
+        });
+    }
+
+    // Cache Keys
+    private static function cacheKey()
+    {
+        Cache::has('data') ? Cache::forget('data') : '';
+    }
+
+    // Logs
+    protected static $logName = 'data';
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
+    }
+}
